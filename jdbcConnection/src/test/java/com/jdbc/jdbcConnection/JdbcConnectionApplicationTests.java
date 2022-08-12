978b76e9888e27fc350444ee2d7b30ca5c65c5e0
Rajveer Mathur <Rajveer.Mathur@BRILLIO.COM>
2022-08-12 16:57:26 +0530

    db created

---
+++ jdbcConnection/src/test/java/com/jdbc/jdbcConnection/JdbcConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbc.jdbcConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
