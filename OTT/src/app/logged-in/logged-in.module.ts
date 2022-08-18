c4aef4c5a66f5f6323b09c19d3c86ce596a7c3a4
Aviraj Ranawat <Aviraj.Ranawat@BRILLIO.COM>
2022-08-18 16:42:32 +0530

    Routing in navbar v1

--- OTT/src/app/logged-in/logged-in.module.ts
+++ OTT/src/app/logged-in/logged-in.module.ts
@@ -9,12 +9,16 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 const routes: Routes = [
   {path:"",component: HomeComponent},
+  //{path: 'movie', component: MovieComponent}, //Movie Component can be child of Home and Child activation as well
   {path:"movies/:id",component: MovieComponent},
+  {path: 'about', component: AboutComponent},
+  {path:'profile',component:ProfileComponent},
 ];
 
 
